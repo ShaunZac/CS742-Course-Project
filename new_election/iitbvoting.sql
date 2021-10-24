d3aabfb1b8f34521473dd3e3b3e7b8d937479ac7
Neha1204 <47505785+Neha1204@users.noreply.github.com>
2021-10-24 16:32:16 +0530

    Database Schema modified
    Tables - 
    admin, voters, positions, candidates, votes

--- new_election/iitbvoting.sql
+++ new_election/iitbvoting.sql
@@ -1,48 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 18, 2018 at 07:34 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.1.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `votesystem`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-
-CREATE TABLE `election` (
-  `id` int(11) NOT NULL,
-  `title` text NOT NULL,
-  `starttime` datetime NOT NULL,
-  `endtime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `election` (`id`, `title`, `starttime`, `endtime`) VALUES
-(1, 'Gymkhana', '2021-03-17 12:00:00', '2021-04-02 12:00:00');
-
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
@@ -54,37 +14,93 @@ CREATE TABLE `admin` (
   `created_on` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admin`
---
 
 INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
-(1, 'nurhodelta', '$2y$10$fLK8s7ZDnM.1lE7XMP.J6OuPbQ.DPUVKBo7rENnQY7gYq0xAzsKJy', 'Neovic', 'Devierte', 'facebook-profile-image.jpeg', '2018-04-02');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'admin', 'facebook-profile-image.jpeg', '2021-10-09');
 
---
--- Indexes for table `admin`
---
-ALTER TABLE `election`
-  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `election`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+CREATE TABLE `voters` (
+  `id` int(11) NOT NULL,
+  `voters_id` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `photo` varchar(150) NOT NULL,
+  `voted` BIT NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `voters` (`id`, `voters_id`, `password`, `firstname`, `lastname`, `photo`) VALUES
+(1, 'voter', '$2y$10$MidkIPVQblBUrevLR.WHseOOQB9X0IiCtHdUf3W1uGTNNDBNYnMh6', 'Voter', 'voter', 'facebook-profile-image.jpeg');
+
+
+CREATE TABLE `candidates` (
+  `id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `photo` varchar(150) NOT NULL,
+  `platform` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `positions` (
+  `id` int(11) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `max_vote` int(11) NOT NULL,
+  `priority` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL,
+  `voters_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `ciphertext` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for table `admin`
---
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  
+ALTER TABLE `voters`
+  ADD PRIMARY KEY (`id`);
 
 
+ALTER TABLE `voters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `candidates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `positions`
+--
+ALTER TABLE `positions`
+  ADD PRIMARY KEY (`id`);
+
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`id`);
+  
+  ALTER TABLE `candidates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `positions`
+--
+ALTER TABLE `positions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+ALTER TABLE `votes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+COMMIT;
+
