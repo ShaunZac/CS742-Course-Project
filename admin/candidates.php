3a05451256191ae652ed45b0c8a44d640a70fd83
Neha1204 <47505785+Neha1204@users.noreply.github.com>
2021-10-24 17:11:15 +0530

    Update candidates.php

--- admin/candidates.php
+++ admin/candidates.php
@@ -61,7 +61,6 @@
                 </thead>
                 <tbody>
                   <?php
-				    $conn = new mysqli('localhost', 'root', '', 'sports');
                     $sql = "SELECT *, candidates.id AS canid FROM candidates LEFT JOIN positions ON positions.id=candidates.position_id ORDER BY positions.priority ASC";
                     $query = $conn->query($sql);
                     while($row = $query->fetch_assoc()){
