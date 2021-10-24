ae99a943ee0b10b0982ab756b6d8af282d01c8f4
Neha1204 <47505785+Neha1204@users.noreply.github.com>
2021-10-24 17:17:15 +0530

    Update positions_row.php

--- admin/positions_row.php
+++ admin/positions_row.php
@@ -3,8 +3,7 @@
 
 	if(isset($_POST['id'])){
 		$id = $_POST['id'];
-		
-		$conn = new mysqli('localhost', 'root', '', 'sports');
+
 		$sql = "SELECT * FROM positions WHERE id = '$id'";
 		$query = $conn->query($sql);
 		$row = $query->fetch_assoc();
