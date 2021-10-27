a8bb28ece8e092f1ab4f28115713f91e40048ff9
Neha1204 <47505785+Neha1204@users.noreply.github.com>
2021-10-27 14:51:14 +0530

    Filtering on input parameters

--- voters_add.php
+++ voters_add.php
@@ -1,27 +1,48 @@
 <?php
-	include 'includes/session.php';
+	include 'admin/includes/session.php';
 
 	if(isset($_POST['add'])){
-		$firstname = $_POST['firstname'];
-		$lastname = $_POST['lastname'];
-		$voter = $_POST['votersid'];
+		
+		$firstname = strip_tags($_POST['firstname']);
+		$lastname = strip_tags($_POST['lastname']);
+		$voter = strip_tags($_POST['votersid']);
 		$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 		$filename = $_FILES['photo']['name'];
 		if(!empty($filename)){
 			move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);	
 		}
-	
-		$sql = "INSERT INTO voters (voters_id, password, firstname, lastname, photo) VALUES ('$voter', '$password', '$firstname', '$lastname', '$filename')";
-		if($conn->query($sql)){
+		
+		$pos = strpos($firstname, "'");
+		if($pos !== false) $firstname = substr($firstname, 0, $pos);
+		
+		$pos = strpos($lastname, "'");
+		if($pos !== false) $lastname = substr($lastname, 0, $pos);
+		
+		$pos = strpos($voter, "'");
+		if($pos !== false) $voter = substr($voter, 0, $pos);
+		
+		$sql = "SELECT * FROM voters WHERE voters_id = '$voter'";
+		$query = $conn->query($sql);
+		
+		if($query->num_rows > 1){
+			$_SESSION['error'] = "$voter already present, Plz select another VOTERS ID" ;
+			header('location: register.php');
 		}
 		else{
-			$_SESSION['error'] = $conn->error;
+			$sql = "INSERT INTO voters (voters_id, password, firstname, lastname, photo) VALUES ('$voter', '$password', '$firstname', '$lastname', '$filename')";
+			if($conn->query($sql)){
+				header('location: index.php');
+			}
+			else{
+				$_SESSION['error'] = $conn->error;
+				header('location: register.php');
+			}
 		}
-
 	}
 	else{
-		$_SESSION['error'] = 'Fill up register form first';
+		$_SESSION['error'] = 'Fill up add form first';
+		header('location: register.php');
 	}
 
-	header('location: index.php');
+	
 ?>
