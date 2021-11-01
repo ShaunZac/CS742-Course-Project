3b11b7b3ae4e24f0b8af123a403b8a1b6e651081
Neha1204 <47505785+Neha1204@users.noreply.github.com>
2021-11-01 12:04:49 +0530

    Encrypt Decrypt Vote Tally done

--- paillier.py
+++ paillier.py
@@ -2,13 +2,14 @@
 
 from phe import paillier
 import sys
+import pymysql
 
 class Paillier:
-    def __init__(self):
-        public_key, private_key = paillier.generate_paillier_keypair()
+    def __init__(self, public_key, private_key):
+        #public_key, private_key = paillier.generate_paillier_keypair()
         self.public_key = public_key
-        self.private_key = private_key
-
+        self.private_key = private_key 
+        
     def encrypt(self, message):
         return self.public_key.encrypt(message)
 
@@ -21,16 +22,45 @@ class Paillier:
             sum_ciphertext = sum_ciphertext + ciphertext
         return sum_ciphertext
 
+n = int(sys.argv[2])
+p = int(sys.argv[3])
+q = int(sys.argv[4])
+public_key = paillier.PaillierPublicKey(n)
+private_key = paillier.PaillierPrivateKey(public_key, p, q)
+paillier_npq = Paillier(public_key, private_key)
 
-trial = Paillier()
-arg = int(sys.argv[1])
-num1 = trial.encrypt(arg)
-num1 = num1.ciphertext()
-print(str(num1))
+if(int(sys.argv[1]) == 1):
+    msg = int(sys.argv[5])
+    a = public_key.encrypt(msg)
+    c = a.ciphertext()
+    print(c)
+else:
+    cId = sys.argv[5]
+    db = pymysql.connect(host = 'localhost', user = 'root', password = '', database = 'iitbvoting')
+    cursor = db.cursor()
+    cipherArray = []
 
-#num2 = trial.encrypt(200)
+    # execute SQL query using execute() method.
+    if(int(sys.argv[1]) == 2):
+        sql = "SELECT ciphertext from en_votes where candidate_id = " + cId
+    else:
+        sql = "SELECT ciphertext from en_votes where voter_id = " + cId
+    
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Fetch all the rows in a list of lists.
+        results = cursor.fetchall()
+        for row in results:
+            d = paillier.EncryptedNumber(public_key, int(row[0]))
+            cipherArray.append(d)
+          
+    except:
+        print ("Error: unable to fetch data")
 
-#public_key_trial = paillier.PaillierPublicKey(num1.public_key.n)
-#num3 = paillier.EncryptedNumber(public_key_trial, num1.ciphertext)
-
-#trial.decrypt(trial.add_encrypted([num1, num2]))
+    # disconnect from server
+    db.close()
+        
+    sumMsg = paillier_npq.decrypt(paillier_npq.add_encrypted(cipherArray))
+    print(sumMsg)
+    
